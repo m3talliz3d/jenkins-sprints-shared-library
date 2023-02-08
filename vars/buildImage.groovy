239ff28e52a59bdd7676b2c5e630a3b74b1e98a0
Yahia Mohamed <105178571+YahiaMohamed90@users.noreply.github.com>
2023-02-09 00:30:56 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-     pipeline {
-    agent any
-    stages {
-        stage('Build') {
-            steps {
-                sh 'docker build -t yahia90/devops:my-appv2 .'
-            }
-     
+    
+    sh 'docker build -t yahia90/devops:my-appv2 .'    
 }
