60074996cb7c3480e48bea27d0c0e9bd139e72b6
Yahia Mohamed <105178571+YahiaMohamed90@users.noreply.github.com>
2023-02-09 00:11:58 +0200

    Delete loginDocker.groovy

--- vars/loginDocker.groovy
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-
-  withCredentials([usernamePassword(credentialsId:"docker-hub",usernameVariable:"USERNAME",passwordVariable:"PASSWORD")]){       
-                sh 'docker login --username $USERNAME --password $PASSWORD'
-               
-}
