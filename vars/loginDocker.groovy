751ef026cf720fb22b1134e95b1911b450d80746
Yahia Mohamed <105178571+YahiaMohamed90@users.noreply.github.com>
2023-02-09 12:57:05 +0200

    Update loginDocker.groovy

--- vars/loginDocker.groovy
+++ vars/loginDocker.groovy
@@ -1,5 +1,6 @@
 def call() {
-
+  
+  echo " login to dockerhub repo "
   withCredentials([usernamePassword(credentialsId:"docker-hub",usernameVariable:"USERNAME",passwordVariable:"PASSWORD")]){       
                 sh 'docker login --username $USERNAME --password $PASSWORD'
                
