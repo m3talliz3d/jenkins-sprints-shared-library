edd3c66e62240a3a6a4a5a765032b014a1794e5b
A-Elfiiky <117025440+A-Elfiiky@users.noreply.github.com>
2023-02-16 20:11:38 +0200

    Delete vars directory

--- vars/buildImage.groovy
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-    
-    echo " build dockerfile "
-    sh " docker build -t yahia90/devops:${env.BUILD_NUMBER} . "    
-}
--- vars/pushImage.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-def call() {
-     echo " push image to dockerhub repo " 
-     sh "docker push  yahia90/devops:${env.BUILD_NUMBER}"
-}
